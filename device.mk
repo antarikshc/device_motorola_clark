3eecb14cad6b756a69df7eb813959eb4f30248f0
Antariksh C <antarikshc@gmail.com>
2018-02-05 19:11:55 +0530

    Build wificond and wifilogd

--- device.mk
+++ device.mk
@@ -277,4 +277,6 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf \
-    android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service \
+    wificond \
+    wifilogd
