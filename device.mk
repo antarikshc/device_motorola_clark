ef3bd88fe636578c4f692779dcacb6c679aac532
Antariksh C <antarikshc@gmail.com>
2018-02-05 23:18:03 +0530

    HAL Build dummy base and manager

--- device.mk
+++ device.mk
@@ -126,6 +126,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/manifest.xml:system/vendor/manifest.xml \
     $(LOCAL_PATH)/configs/compatibility_matrix.xml:system/vendor/compatibility_matrix.xml
 
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
