LineageOS/android_device_xiaomi_libra

light: turn into a binderized hal
Change-Id: Idd0b51c2dc8ba58d5dd1c520318026d2fda8ecac

device.mk
@@ -209,8 +209,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.msm8992
+    android.hardware.light@2.0-service.libra
 
 # Media
 PRODUCT_PACKAGES += \

antarikshc/device_motorola_clark

device.mk
@@ -150,7 +150,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.msm8992
+    android.hardware.light@2.0-service.clark
 
 # Media
 PRODUCT_PACKAGES += \
@@ -280,3 +280,4 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     wificond \
     wifilogd
+