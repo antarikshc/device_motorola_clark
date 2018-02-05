2ef174d7de009356531a40e205b6b8a9fc52ed79
Antariksh C <antarikshc@gmail.com>
2018-02-05 16:11:18 +0530

    CameraWrapper: fix for O

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,7 +9,10 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libgui libutils
+    libhardware liblog libgui libutils android.hidl.token@1.0-utils libgui libsensor
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect libbase
 
 LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_RELATIVE_PATH := hw
