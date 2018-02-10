0f4bc3ed7c1eb38aee937ee6e326ef819e28775f
Antariksh C <antarikshc@gmail.com>
2018-02-10 18:31:28 +0530

    Don't use the STL in C projects. * The libc++ update doesn't play nicely with GCC compiling C sources. * Add missing liblog dependencies

--- modules/camera/QCamera2/stack/mm-camera-test/Android.mk
+++ modules/camera/QCamera2/stack/mm-camera-test/Android.mk
@@ -115,6 +115,7 @@ LOCAL_SHARED_LIBRARIES:= \
          libcutils libdl libmmcamera_interface
 
 LOCAL_CLANG       := false
+LOCAL_CXX_STL 	  := none
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_32_BIT_ONLY := $(BOARD_QTI_CAMERA_32BIT_ONLY)
@@ -221,9 +222,10 @@ endif
 LOCAL_CFLAGS += -Wall -Wextra -Werror
 
 LOCAL_SHARED_LIBRARIES:= \
-         libcutils libdl libmmcamera_interface
+         libcutils libdl libmmcamera_interface liblog
 
 LOCAL_CLANG       := false
+LOCAL_CXX_STL     := none
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_32_BIT_ONLY := $(BOARD_QTI_CAMERA_32BIT_ONLY)
