14c44c63c9168d3231a48782f46b0180459ce433
Antariksh C <antarikshc@gmail.com>
2018-02-24 19:14:21 +0000

    Revert "Camera: Add QCameraFlash config"
    This reverts commit af253d43316c3a6a01d79a1f271e2ff4678d15cc.

--- modules/camera/QCamera2/Android.mk
+++ modules/camera/QCamera2/Android.mk
@@ -7,7 +7,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
         util/QCameraCmdThread.cpp \
         util/QCameraQueue.cpp \
-        util/QCameraFlash.cpp \
         QCamera2Hal.cpp \
         QCamera2Factory.cpp
 
